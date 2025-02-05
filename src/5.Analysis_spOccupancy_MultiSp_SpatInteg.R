66c6a14fe28c4c63d5deef73aefe7e62547caae9
Viviane Zulian <81876239+vivizulian@users.noreply.github.com>
2025-02-05 10:07:15 -0500

    commenting text

--- src/5.Analysis_spOccupancy_MultiSp_SpatInteg.R
+++ src/5.Analysis_spOccupancy_MultiSp_SpatInteg.R
@@ -272,10 +272,10 @@ str(data.int)
 ##############################################################################
 ##############################################################################
 ##### Run model
-<<<<<<< Updated upstream
+##<<<<<<< Updated upstream
 ##############################################################################
-=======
->>>>>>> Stashed changes
+##=======
+##>>>>>>> Stashed changes
 print("\n\n###### running model...")
 occ.formula.int <- ~  scale(Depth) + scale(SST) +  I(scale(SST)^2) +
   scale(Chlor) + I(scale(Chlor)^2) + scale(TSM) + scale(SSH)
@@ -359,10 +359,10 @@ summary(ppc.out.g1)
 ##############################################################################
 print(glue("\n\n###### exporting output for sp {AllSpp[j]}"))
 sink(file = paste("Bayesian_p-valueFull", seasonName, "_", AllSpp[j],".txt", sep = ""))
-<<<<<<< Updated upstream
-=======
+##<<<<<<< Updated upstream
+##=======
 print(glue("\n\n###### exporting output for sp {AllSpp[j]}"))
->>>>>>> Stashed changes
+##>>>>>>> Stashed changes
 summary(ppc.out.g1)
 sink(file = NULL)
   
@@ -500,11 +500,11 @@ SSH.pred <- (Grid_OccEnv[[SSH]] - mean(data.int$occ.covs[, predInd4])) / sd(data
 # X.0 <- cbind(1, Depth.pred, SST.pred,  SST.pred^2, Chlor.pred, Chlor.pred^2, SSH.pred)  #Depth.pred
 X.0 <- cbind(1, Depth.pred, SST.pred,  SST.pred^2, Chlor.pred, Chlor.pred^2, TSM.pred, SSH.pred)  #Depth.pred
 coords.0 <- as.matrix(Grid_OccEnv[, c('X', 'Y')])
-<<<<<<< Updated upstream
+##<<<<<<< Updated upstream
 print("\n\n###### head(X.0) :")
-=======
+##=======
 #print("\n\n###### head(X.0) :")
->>>>>>> Stashed changes
+##>>>>>>> Stashed changes
 print(head(X.0))
 out.sp.pred <- predict(out.sp.int, X.0, coords.0, verbose = FALSE) # Spatial
 # out.sp.pred <- predict(out.sp.int, X.0) # Non-spatial
@@ -518,16 +518,16 @@ plot.dat <- data.frame(x = Grid_OccEnv$X,
 
 class(FullGrid)
 plot.grid <- cbind(st_as_sf(FullGrid), plot.dat$mean.psi, plot.dat$sd.psi)
-<<<<<<< Updated upstream
+##<<<<<<< Updated upstream
 ##############################################################################
 ##############################################################################
 ## Export predictions
 ##############################################################################
 ## predictive map as shapefile
-=======
+##=======
 
 ## Export predictive map as shapefile
->>>>>>> Stashed changes
+##>>>>>>> Stashed changes
 print('\n\n###### exporting map as shapefile...')
 spName_shape <- chartr(" ", "_", AllSpp[j])
 file_name_shape = paste("SDM_Shape_", seasonName, "_", spName_shape, ".shp", sep="")
